170cc43f1c316a149f0639f7c62bf264163f7f11
selametsamli <selamet96@gmail.com>
2019-07-11 19:49:16 +0300

    using external template files with context data

---
+++ templates.py
@@ -0,0 +1,30 @@
+import os
+
+
+def get_template_path(path):
+    file_path = os.path.join(os.getcwd(),path)
+    if not os.path.isfile(file_path):
+        raise Exception("This is not a valid template path %s"%(file_path))
+    return file_path
+
+
+# çektiğimiz dosyayı okumayı sağlarız.
+def get_template(path):
+    file_path = get_template_path(path)
+    return open(file_path).read()
+
+def render_context(template_string,context):
+    return template_string.format(**context)
+
+file_ = 'templates/email_message.txt'
+file_html = 'templates/email_message.html'
+template = get_template(file_)
+template_html = get_template(file_html)
+context = {
+    "name":"selamet",
+    "amount":"53",
+    "date":"abs"
+
+}
+#template_text = get_template(file_).format(name="Selamet",date='abc',total= None, amount='53')
+print(render_context(template_html,context))
